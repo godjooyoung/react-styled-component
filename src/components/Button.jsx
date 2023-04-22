6889dcb807555ec2acac72c162559e710df9bd2f
godjooyoung <xolovesjoo36@naver.com>
2023-04-22 16:02:20 +0900

    App.js h1엘리먼트 제거 Button 컴포넌트 스타일 공통으로 이동 Buttons 컴포넌트 스타일 공통으로 이동

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,79 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
 import ButtonIcon from './ButtonIcon';
-
+import * as commonSt from './StyledComponents'
 
 function Button(props) {
     return (
         <>
             {
-                props.size == 'l'?<StBtnL type={props.type} onClick={props.onClick}><StContainer>{props.children}{props.hasIcon?<ButtonIcon type={props.hasIcon}/>:''}</StContainer></StBtnL> : (props.size == 'm' ? <StBtnM type={props.type} onClick={props.onClick}>{props.children}</StBtnM> : <StBtnS type={props.type} onClick={props.onClick}>{props.children}</StBtnS>)
+                props.size == 'l'?<commonSt.StBtnL type={props.type} onClick={props.onClick}><commonSt.StTtIcnContain>{props.children}{props.hasIcon?<ButtonIcon type={props.hasIcon}/>:''}</commonSt.StTtIcnContain></commonSt.StBtnL> : (props.size == 'm' ? <commonSt.StBtnM type={props.type} onClick={props.onClick}>{props.children}</commonSt.StBtnM> : <commonSt.StBtnS type={props.type} onClick={props.onClick}>{props.children}</commonSt.StBtnS>)
             }
         </>
     );
 }
 
+// 공통스타일 분리후 적용
 // 버튼 스타일
-const StBtnL = styled.button`
-  height : 50px;
-  width : 200px;
-  border-radius: 8px;
-  border: 3px solid ${(props) => {
-        return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
-    }};
-  background-color: rgb(255, 255, 255);
-  color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
-  font-weight: 900;
-  cursor: pointer;
-  &:active{
-    border: 3px solid ${(props) => {
-        return props.type == "primary" ? '#EBDEDE' : '#D1CCEB'
-    }};
-    background-color: rgb(0,0,0,0.1)
-  }
-`
+// const StBtnL = styled.button`
+//   height : 50px;
+//   width : 200px;
+//   border-radius: 8px;
+//   border: 3px solid ${(props) => {
+//         return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
+//     }};
+//   background-color: rgb(255, 255, 255);
+//   color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
+//   font-weight: 900;
+//   cursor: pointer;
+//   &:active{
+//     border: 3px solid ${(props) => {
+//         return props.type == "primary" ? '#EBDEDE' : '#D1CCEB'
+//     }};
+//     background-color: rgb(0,0,0,0.1)
+//   }
+// `
 
-const StBtnM = styled.button`
-  height : 45px;
-  width : 130px;
-  border-radius: 8px;
-  border: none;
-  background-color: ${(props) => {
-        return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
-    }};
-  color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
-  cursor: pointer;
-  &:active{
-    background-color: ${(props)=>{
-        return props.type == 'primary' ? '#EBDEDE' : '#D1CCEB'
-    }}
-  }
-`
+// const StBtnM = styled.button`
+//   height : 45px;
+//   width : 130px;
+//   border-radius: 8px;
+//   border: none;
+//   background-color: ${(props) => {
+//         return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
+//     }};
+//   color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
+//   cursor: pointer;
+//   &:active{
+//     background-color: ${(props)=>{
+//         return props.type == 'primary' ? '#EBDEDE' : '#D1CCEB'
+//     }}
+//   }
+// `
 
-const StBtnS = styled.button`
-  height : 40px;
-  width : 100px;
-  border-radius: 8px;
-  border: none;
-  background-color: ${(props) => {
-        return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
+// const StBtnS = styled.button`
+//   height : 40px;
+//   width : 100px;
+//   border-radius: 8px;
+//   border: none;
+//   background-color: ${(props) => {
+//         return props.type == "primary" ? '#FFF2F2' : '#E5E0FF'
 
-    }};
-  color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
-  cursor: pointer;
-  &:active{
-    background-color: ${(props)=>{
-        return props.type == 'primary' ? '#EBDEDE' : '#D1CCEB'
-    }}
-  }
-`
+//     }};
+//   color : ${(props) => { return props.type == 'negative' ? '#98B1F3' : '#5E72BF' }};
+//   cursor: pointer;
+//   &:active{
+//     background-color: ${(props)=>{
+//         return props.type == 'primary' ? '#EBDEDE' : '#D1CCEB'
+//     }}
+//   }
+// `
 
-// 버튼 내부 텍스트 및 아이콘 정렬
-const StContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 7px;
-`
+// // 버튼 내부 텍스트 및 아이콘 정렬
+// const StTtIcnContain = styled.div`
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     gap: 7px;
+// `
 
 export default Button;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,41 +1,45 @@
 import React from 'react';
 import Button from './Button';
-import styled from 'styled-components';
+import * as commonSt from './StyledComponents'
 
 function Buttons() {
     const btnAlertEventHandler = (e)=>{
         alert("버튼을 만들어보세요.")
     }
     const btnPromtEventHandler = (e)=>{
-        
+        let promptStr = prompt('어렵나요?')
+        console.log("prompt input",promptStr)
+
     }
     return (
-        <StBtnBox>
-            <h1>Button</h1>
-            <StBtnBoxHAlign>
+        <commonSt.StBtnBox>
+            <commonSt.StTitle>Button</commonSt.StTitle>
+            <commonSt.StBtnBoxHAlign>
                 <Button size="l" type="primary" hasIcon="arrow" onClick={btnAlertEventHandler}>Large Primary Button</Button>
                 <Button size="m" type="primary">Medium</Button>
                 <Button size="s" type="primary">small</Button>
-            </StBtnBoxHAlign>
-            <StBtnBoxHAlign>
-                <Button size="l" type="negative" hasIcon="alarm">Large Negative Button</Button>
+            </commonSt.StBtnBoxHAlign>
+            <commonSt.StBtnBoxHAlign>
+                <Button size="l" type="negative" hasIcon="alarm" onClick={btnPromtEventHandler}>Large Negative Button</Button>
                 <Button size="m" type="negative">Medium</Button>
                 <Button size="s" type="negative">small</Button>
-            </StBtnBoxHAlign>
-        </StBtnBox>
+            </commonSt.StBtnBoxHAlign>
+        </commonSt.StBtnBox>
     );
 }
 
-const StBtnBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
+// 공통스타일로 분리하여서 가져옴. 
 
-const StBtnBoxHAlign = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-`
+// const StBtnBox = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     gap: 10px;
+// `
+
+// const StBtnBoxHAlign = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     gap: 10px;
+// `
 
 export default Buttons;
